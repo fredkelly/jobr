1d04d23a472bcde6b8577aeadd3069c1298cfece
Danyal Prout <me@dany.al>
2012-11-10 20:33:07 +0000

    Test Data

---
+++ data/TestData.sql
@@ -0,0 +1,33 @@
+INSERT INTO companies (name) VALUES ('Apple Inc.');
+
+INSERT INTO jobs (title, description, closing_date, url, sponsored, type, created_at, area_id, company_id, job_type_id) VALUES ('Drupal Developer', 'Drupal Developer - London - £60k. URGENT!
+
+This has to be one of the most exciting positions in the market today, this is a unique opportunity with the one of the countries fasted growing dot.com companies. They are already a household name, sponsoring top football teams and in the Sunday Times Tech Track top 100 list!
+
+As their team expands they have a requirement for a Drupal Developer to join their team of rockstar developers. Here you will you will be involved in the cutting edge development of the Drupal frontend UI/UE for a brand new product soon to be launched while also involved in ensuring the future success of Europes fastest growing technology business. 
+
+Responsibilities will include: 
+* Development of the Drupal frontend UI/UE 
+* Participating in the architecture of new features and implementing them 
+* Administration and management of a Global Drupal website 
+* Involved in launch of brand new products 
+
+You will have: 
+* A passion for frontend web development, web optimisation, standards compliant code 
+* Drupal module development and theming 
+* Love open source and is always up-to-date on the latest and greatest web technologies 
+* A proactive approach to problem solving and good communication skills are essential 
+* Experience working with international sites an advantage 
+* Enjoy working in a fast paced environment 
+
+Core skills include Drupal, PHP/SQL/Linux/Apache (LAMP) while anyone with experience of JavaScript, HTML/CSS and continuous integration will have a distinct advantage.
+
+This is a great time to join, for a Drupal Developer there is nothing like this in the market today. If this role is of interest please do send me through an up to date CV and follow up with a call to the number below.', to_date('13 November 2013', 'DD Month YYYY'), 'http://www.jobsite.co.uk/', FALSE, 1, to_date('08 November 2012', 'DD Month YYYY'), 7, 1, 7);
+
+INSERT INTO jobs (title, description, closing_date, url, sponsored, type, created_at, area_id, company_id, job_type_id) VALUES ('SOFTWARE PROGRAMMER', 'SOFTWARE PROGRAMMER CLASSIC ASP - £26000 MINIMUM + BENEFITS - LINCOLN
+
+Based in Louth, close to the vibrant city of Lincoln and within easy commuting distance of Grimsby, Boston, Nottingham and Scunthorpe, this company is a highly creative and forward thinking organisation specialising in the development of new software management products mainly within the health and educational sectors which has gone from strength to strength, expanding its client base and developing new ideas.
+
+The company prides itself on its reputation of delivering creative, high quality, practical solutions, exceptional customer service, proactive ideas, innovative strategies and fresh approaches to problems, coupled with a particular attention to detail which has enabled it to retain an exceptional number of regular and big named clients.
+
+Due to a major expansion the company is looking for additional software programmers (SOC 2132) well-versed in Classic ASP to support legacy systems during the transition of bespoke software to the latest standards. There will be opportunities to learn new software development skills following the transition. Reporting to the Development Director, the role offers a minimum salary of £26k (depending on experience) together with a funded training plan to further develop your skills and utilisation of new technologies. They will be based at the company`s Louth office, but may be required to occasionally travel to the company`s Darlington office. The closing date for applications is 7th December 2012.', to_date('19 November 2013', 'DD Month YYYY'), 'http://www.jobsite.co.uk/', TRUE, 1, to_date('09 November 2012', 'DD Month YYYY'), 7, 1, 7);
