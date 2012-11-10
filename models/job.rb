1bbd4a843c06beefdf8ae2a3aa4f090394226011
Sam Bell <samueljamesbell@gmail.com>
2012-11-10 11:46:51 +0000

    Job model

---
+++ models/job.rb
@@ -0,0 +1,15 @@
+class Job
+    include DataMapper::Resource
+    
+    property :id, Serial
+    property :title, String
+    property :description, String
+    property :closing_date, Date
+    property :url, String
+    property :sponsored, Boolean
+    property :type, Enum[:permanent, :temporary], :default => :permanent
+
+    property :created_at, DateTime
+    property :updated_at, DateTime
+end
+
