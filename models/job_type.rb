dacc1467cac2963505ad9137254a03ed91ae3dea
Sam Bell <samueljamesbell@gmail.com>
2012-11-10 18:11:09 +0000

    a

--- models/job_type.rb
+++ models/job_type.rb
@@ -2,7 +2,7 @@ class JobType
     include DataMapper::Resource
 
     property :id, Serial
-    property :name, String, :length => 255
+    property :name, String, :length => 256
 
     has n, :jobs
 end
