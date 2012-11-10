993d61f9e4a34374922b626768a8a81913b1e4d0
Sam Bell <samueljamesbell@gmail.com>
2012-11-10 16:13:40 +0000

    Import script

---
+++ data/import.rb
@@ -0,0 +1,10 @@
+require 'pg'
+conn = PGconn.open('ec2-54-243-62-232.compute-1.amazonaws.com', 5432, '', '', 'dcqi3qigqol656', 'jezljnhcjqurqt', 'GKCDXOuHvlLjUFFlxnpzGZSJ_x')
+
+areas = File.new('data/Area.sql', 'r')
+job_types = File.new('data/JobType.sql', 'r')
+vacancies = File.new('data/Vacancy.sql', 'r')
+
+conn.exec(areas)
+conn.exec(job_types)
+conn.exec(vacancies)
