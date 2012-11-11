24e118a1b9b729d547db3578e01a2818857d9dea
Danyal Prout <me@dany.al>
2012-11-11 15:33:50 +0000

    Jobs

---
+++ data/Companies.sql
@@ -0,0 +1,8 @@
+INSERT INTO companies (name) VALUES ('The Wharf');
+INSERT INTO companies (name) VALUES ('Cafe Rouge');
+INSERT INTO companies (name) VALUES ('Hilton Hotel');
+INSERT INTO companies (name) VALUES ('Marks and spencer');
+INSERT INTO companies (name) VALUES ('Premeir Inn');
+INSERT INTO companies (name) VALUES ('The Lowry Hotel');
+INSERT INTO companies (name) VALUES ('Undisclosed');
+INSERT INTO companies (name) VALUES ('Tech Hub');
